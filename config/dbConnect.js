9486c8cf51e97cf01150d05ddf2e57665db53738
Ahmed Khaled <ahmed1999khaledaboamer1999@gmail.com>
2026-09-07 15:34:35 +0300

    Remove commented error handling from database connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
     mongoose.connect(process.env.DB_URI).then((conn) => {
       console.log(`Database Connected ${conn.connection.host}`);
     });
-     });
+    // .catch((err) => {
+    //   console.log(`database error ${err}`);
+    //   process.exit(1);
+    // });
   };
 
 module.exports=dbConnect;
