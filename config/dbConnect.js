936e7b370735ed07d1ab3b5989f88a0dffa28989
Ahmed Khaled <ahmed1999khaledaboamer1999@gmail.com>
2026-09-07 15:51:18 +0300

    Add config.env file and clean up dbConnect.js error handling

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -4,10 +4,7 @@ const mongoose = require('mongoose');
     mongoose.connect(process.env.DB_URI).then((conn) => {
       console.log(`Database Connected ${conn.connection.host}`);
     });
-    // .catch((err) => {
-    //   console.log(`database error ${err}`);
-    //   process.exit(1);
-    // });
+     
   };
 
 module.exports=dbConnect;
